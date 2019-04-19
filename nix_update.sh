96b5e4a72cd65efc5e6462f5fb5d23fd29bccbc6
dacx <5989168+dacx@users.noreply.github.com>
2019-04-19 17:15:13 +0200

    v2.3.0.3

--- nix_update.sh
+++ nix_update.sh
@@ -3,7 +3,7 @@
 TMP_FOLDER=$(mktemp -d)
 COIN_DAEMON='/usr/local/bin/nixd'
 COIN_CLI='/usr/local/bin/nix-cli'
-COIN_REPO='https://github.com/NixPlatform/NixCore/releases/download/v2.3.0.2/nix-2.3.0-x86_64-linux-gnu.tar.gz'
+COIN_REPO='https://github.com/NixPlatform/NixCore/releases/download/v2.3.0.3/nix-2.3.0-x86_64-linux-gnu.tar.gz'
 COIN_NAME='NIX'
 #COIN_BS='http://bootstrap.zip'
 
